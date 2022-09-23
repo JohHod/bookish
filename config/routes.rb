6a709b210ce7436bc4381073b2c24693c7ca201f
JohHod <john.hodkinson@softwire.com>
2022-09-23 15:22:53 +0100

    Add manage page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'manage/index'
   get 'books/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'books/:id', to: 'books#details'
+  get 'manage', to: 'manage#index'
 
   # Defines the root path route ("/")
   root "books#index"
 
-  get 'books/:id', to: 'books#details'
 
 end
