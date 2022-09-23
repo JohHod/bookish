c14a2b27ec263cc3a4ffa5de590f3b18c48561bf
JohHod <john.hodkinson@softwire.com>
2022-09-23 14:38:26 +0100

    Added stock message to book details

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,20 @@ class BooksController < ApplicationController
   def details
     @book = Book.find_by_id(params[:id])
     @copies = []
+    available_stock = 0
     Copy.find_each do |copy|
-      if copy.book_id == params[:id].to_i then @copies.push(copy) end
+      if copy.book_id == params[:id].to_i then
+        if copy.due_date.nil? then
+          available_stock += 1
+        else
+          @copies.push(copy)
+        end
+      end
     end
-    puts(@copies)
+    @stock_message = if available_stock == 0
+                       "<p><strong class='has-text-danger'>Out of stock</strong></p>"
+                     else
+                       "<p>#{available_stock} cop#{available_stock > 1 ? 'ies' : 'y'} in stock</p>"
+                     end
   end
 end
